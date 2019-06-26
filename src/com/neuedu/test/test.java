f87857bf485ba9b93144ba5d0a6eca3f6a3cb2a6
LiYan_Git <1258208743@qq.com>
2019-06-26 21:35:03 +0800

    分支上第二次提交

--- src/com/neuedu/test/test.java
+++ src/com/neuedu/test/test.java
@@ -4,5 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("测试");
         System.out.println("分支创建");
+        System.out.println("分支上第二次提交");
+
     }
 }
